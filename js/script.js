ff5233bd075ed80dadbdcb89faa91daf21756ed8
Patryk Majchrzak <pmaj12345@gmail.com>
2024-06-26 09:59:03 +0200

    adding function calculateResult which calls one of the four functions depending of currency we are calculating result from

--- js/script.js
+++ js/script.js
@@ -20,28 +20,74 @@
 
     backgroundButton.addEventListener("click", changeGraphics);
 
-    form.addEventListener("submit", (event) => {
+    form.addEventListener("submit", calculateResult)
+
+    const calculateResultfromPLN = (amount, to) => {
+        const PLNtoEUR = 0.2308;
+        const PLNtoGBP = 0.1950;
+        const PLNtoUSD = 0.2467;
+
+        switch (to) {
+            case "EUR":
+                return calculation = amount * PLNtoEUR;
+            case "GBP":
+                return calculation = amount * PLNtoGBP;
+            case "USD":
+                return calculation = amount * PLNtoUSD;
+        };
+    };
+
+    const calculateResultfromEUR = (amount, to) => {
+        const EURtoPLN = 4.3331;
+        const EURtoGBP = 0.8448;
+        const EURtoUSD = 1.0692;
+
+        switch (to) {
+            case "PLN":
+                return calculation = amount * EURtoPLN;
+            case "GBP":
+                return calculation = amount * EURtoGBP;
+            case "USD":
+                return calculation = amount * EURtoUSD;
+        };
+    };
+
+    const calculateResultfromGBP = (amount, to) => {
+        const GBPtoPLN = 5.1291;
+        const GBPtoEUR = 1.1837;
+        const GBPtoUSD = 1.2656;
+
+        switch (to) {
+            case "PLN":
+                return calculation = amount * GBPtoPLN;
+            case "EUR":
+                return calculation = amount * GBPtoEUR;
+            case "USD":
+                return calculation = amount * GBPtoUSD;
+        };
+    };
+
+    const calculateResultfromUSD = (amount, to) => {
+        const USDtoPLN = 4.0527;
+        const USDtoEUR = 0.9353;
+        const USDtoGBP = 0.7901;
+
+        switch (to) {
+            case "PLN":
+                return calculation = amount * USDtoPLN;
+            case "EUR":
+                return calculation = amount * USDtoEUR;
+            case "USD":
+                return calculation = amount * USDtoGBP;
+        };
+    };
+
+    const calculateResult = (event) => {
         event.preventDefault();
 
         const amount = +amountInput.value;
         const from = currencyFrom.value;
         const to = currencyTo.value;
-
-        const PLNtoEUR = 0.2308;
-        const PLNtoGBP = 0.1950;
-        const PLNtoUSD = 0.2467;
-
-        const EURtoPLN = 4.3331;
-        const EURtoGBP = 0.8448;
-        const EURtoUSD = 1.0692;
-
-        const GBPtoPLN = 5.1291;
-        const GBPtoEUR = 1.1837;
-        const GBPtoUSD = 1.2656;
-
-        const USDtoPLN = 4.0527;
-        const USDtoEUR = 0.9353;
-        const USDtoGBP = 0.7901;
 
         let calculation;
 
@@ -49,57 +95,18 @@
             calculation = amount
         }
         else if (from === "PLN") {
-            switch (to) {
-                case "EUR":
-                    calculation = amount * PLNtoEUR;
-                    break;
-                case "GBP":
-                    calculation = amount * PLNtoGBP;
-                    break;
-                case "USD":
-                    calculation = amount * PLNtoUSD;
-                    break;
-            };
+            calculateResultfromPLN(amount, to);
         }
         else if (from === "EUR") {
-            switch (to) {
-                case "PLN":
-                    calculation = amount * EURtoPLN;
-                    break;
-                case "GBP":
-                    calculation = amount * EURtoGBP;
-                    break;
-                case "USD":
-                    calculation = amount * EURtoUSD;
-                    break;
-            };
+            calculateResultfromEUR(amount, to);
         }
         else if (from === "GBP") {
-            switch (to) {
-                case "PLN":
-                    calculation = amount * GBPtoPLN;
-                    break;
-                case "EUR":
-                    calculation = amount * GBPtoEUR;
-                    break;
-                case "USD":
-                    calculation = amount * GBPtoUSD;
-                    break;
-            };
+            calculateResultfromGBP(amount, to);
         }
         else if (from === "USD") {
-            switch (to) {
-                case "PLN":
-                    calculation = amount * USDtoPLN;
-                    break;
-                case "EUR":
-                    calculation = amount * USDtoEUR;
-                    break;
-                case "USD":
-                    calculation = amount * USDtoGBP;
-                    break;
-            };
+            calculateResultfromUSD(amount, to)
         };
-        resultText.innerText = `${amount.toFixed(2)} ${from} to ${calculation.toFixed(2)} ${to}`;
-    });
+    };
+
+    resultText.innerText = `${amount.toFixed(2)} ${from} to ${calculation.toFixed(2)} ${to}`;
 };
